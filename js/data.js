47603b1b13f2edfb31f614cef7dadd001cdae745
edtan094 <etan094@gmail.com>
2022-01-07 17:08:18 -0800

    create a search icon on the top right of the top. create a data variable in data.js with the following property key: view, saved

--- js/data.js
+++ js/data.js
@@ -1,1 +1,15 @@
 /* exported data */
+var data = {
+  view: 'home-page',
+  saved: []
+};
+
+var previousEntriesJSON = localStorage.getItem('entry');
+if (previousEntriesJSON !== null) {
+  data = JSON.parse(previousEntriesJSON);
+}
+
+window.addEventListener('beforeunload', function () {
+  var savedData = JSON.stringify(data);
+  localStorage.setItem('entry', savedData);
+});
