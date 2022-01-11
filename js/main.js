e92b2a0a09bcbf02c6d545a854acec61700d929d
edtan094 <etan094@gmail.com>
2022-01-10 18:03:43 -0800

    once the user hits save, ensure that the object in data.clicked is pushed to the saved array. ensure that the saved page is generated using the data from the saved array within data. ensure that the saved page looks just like the home search page

--- js/main.js
+++ js/main.js
@@ -142,6 +142,7 @@ function switchViews(event) {
 }
 document.addEventListener('click', switchViews);
 
+var $savedWeaponList = document.querySelector('#saved-weapon-list');
 function previousDataView(data) {
   for (var viewIndex = 0; viewIndex < $allViews.length; viewIndex++) {
     if (data.view === $allViews[viewIndex].getAttribute('data-view')) {
@@ -151,5 +152,21 @@ function previousDataView(data) {
   $imageInDetails.setAttribute('src', data.clicked.image);
   $descriptionInDetails.textContent = data.clicked.description;
   $commonLocationsInDetails.textContent = data.clicked.common_locations;
+
+  if (data.view === 'saved') {
+    for (var i = 0; i < data.saved.length; i++) {
+      $savedWeaponList.appendChild(generateDOM(data.saved[i]));
+    }
+  }
 }
 previousDataView(data);
+
+function saveWeapon(event) {
+  if (event.target.matches('.save-button')) {
+    data.saved.push(data.clicked);
+    $savedWeaponList.appendChild(generateDOM(data.clicked));
+    loopThroughViews('saved');
+  }
+}
+var $saveButton = document.querySelector('.save-button');
+$saveButton.addEventListener('click', saveWeapon);
