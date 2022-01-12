876800049aee739bf66c3a87c0e2583eda62e779
edtan094 <etan094@gmail.com>
2022-01-11 18:48:22 -0800

    ensure that once the delete button is hit by the user, the entry from data.saved is removed. ensure that the weapon is removed from the saved list even after refreshing the page. remove and unnecessary css and console.log.

--- js/main.js
+++ js/main.js
@@ -45,26 +45,25 @@ function generateDOM(info) {
     }
   }
   $paragraphName.textContent = properName;
-  $paragraphName.setAttribute('class', 'margin-top-five font-size-17 weapon-name weapons');
+  $paragraphName.setAttribute('class', 'margin-top-five font-size-17 weapon-name weapons margin-bottom-0');
   $columnHalfDiv.appendChild($paragraphName);
 
   var $paragraphLocation = document.createElement('p');
   $paragraphLocation.textContent = 'Common Locations';
-  $paragraphLocation.setAttribute('class', 'margin-bottom-five font-size-15 weapons');
+  $paragraphLocation.setAttribute('class', 'margin-bottom-five font-size-15 weapons margin-top-0');
   $columnHalfDiv.appendChild($paragraphLocation);
 
   var $ul = document.createElement('ul');
-  $ul.setAttribute('class', 'margin-top-five weapons margin-bottom-five');
+  $ul.setAttribute('class', 'margin-top-five weapons margin-bottom-five max-height-48');
   $columnHalfDiv.appendChild($ul);
-  if (data.view === 'saved') {
-    var $deleteButton = document.createElement('button');
-    var $divForDeleteButton = document.createElement('div');
-    $deleteButton.setAttribute('class', 'delete-button');
-    $deleteButton.textContent = 'Delete';
-    $divForDeleteButton.setAttribute('class', 'row justify-end');
-    $divForDeleteButton.appendChild($deleteButton);
-    $columnHalfDiv.appendChild($divForDeleteButton);
-  }
+  var $deleteButton = document.createElement('button');
+  var $divForDeleteButton = document.createElement('div');
+  $deleteButton.setAttribute('class', 'delete-button');
+  $deleteButton.textContent = 'Delete';
+  $divForDeleteButton.setAttribute('class', 'hidden view row justify-end');
+  $divForDeleteButton.setAttribute('data-view', 'saved');
+  $divForDeleteButton.appendChild($deleteButton);
+  $columnHalfDiv.appendChild($divForDeleteButton);
 
   if (info.common_locations === null) {
     var $unknownLi = document.createElement('li');
@@ -112,11 +111,7 @@ function bringUserToDetailsPage(event) {
     var dataBOTW = xhr.response.data;
     for (var dataIndex = 0; dataIndex < dataBOTW.length; dataIndex++) {
       if (dataBOTW[dataIndex].id === weaponThatWasClickedID) {
-        data.clicked.name = dataBOTW[dataIndex].name;
-        data.clicked.image = dataBOTW[dataIndex].image;
-        data.clicked.description = dataBOTW[dataIndex].description;
-        data.clicked.common_locations = dataBOTW[dataIndex].common_locations;
-        data.clicked.id = dataBOTW[dataIndex].id;
+        data.clicked = dataBOTW[dataIndex];
         $imageInDetails.setAttribute('src', data.clicked.image);
         $descriptionInDetails.textContent = data.clicked.description;
         $commonLocationsInDetails.textContent = data.clicked.common_locations;
@@ -128,9 +123,8 @@ function bringUserToDetailsPage(event) {
 
 document.addEventListener('click', bringUserToDetailsPage);
 
-var $allViews = document.querySelectorAll('.view');
-
 function loopThroughViews(view) {
+  var $allViews = document.querySelectorAll('.view');
   for (var viewIndex = 0; viewIndex < $allViews.length; viewIndex++) {
     if ($allViews[viewIndex].getAttribute('data-view') !== view) {
       $allViews[viewIndex].classList.replace('display', 'hidden');
@@ -149,6 +143,9 @@ function switchViews(event) {
     loopThroughViews('home-page');
   }
   if (event.target.matches('.saved-weapon-list')) {
+    for (var i = 0; i < data.saved.length; i++) {
+      $savedWeaponList.appendChild(generateDOM(data.saved[i]));
+    }
     loopThroughViews('saved');
   }
 }
@@ -156,6 +153,7 @@ document.addEventListener('click', switchViews);
 
 var $savedWeaponList = document.querySelector('#saved-weapon-list');
 function previousDataView(data) {
+  var $allViews = document.querySelectorAll('.view');
   for (var viewIndex = 0; viewIndex < $allViews.length; viewIndex++) {
     if (data.view === $allViews[viewIndex].getAttribute('data-view')) {
       $allViews[viewIndex].classList.replace('hidden', 'display');
@@ -164,21 +162,41 @@ function previousDataView(data) {
   $imageInDetails.setAttribute('src', data.clicked.image);
   $descriptionInDetails.textContent = data.clicked.description;
   $commonLocationsInDetails.textContent = data.clicked.common_locations;
-
   if (data.view === 'saved') {
     for (var i = 0; i < data.saved.length; i++) {
       $savedWeaponList.appendChild(generateDOM(data.saved[i]));
     }
   }
+  loopThroughViews(data.view);
 }
 previousDataView(data);
 
 function saveWeapon(event) {
   if (event.target.matches('.save-button')) {
+    var $allWeapons = document.querySelectorAll('.weapon-name');
+    for (var nameIndex = 0; nameIndex < $allWeapons.length; nameIndex++) {
+      var $weaponsThatAreHiddenFromSearchBar = $allWeapons[nameIndex].closest('.green-card');
+      $weaponsThatAreHiddenFromSearchBar.classList.remove('hidden');
+    }
     data.saved.push(data.clicked);
-    loopThroughViews('saved');
+    var $savedWeaponList = document.querySelector('#saved-weapon-list');
     $savedWeaponList.appendChild(generateDOM(data.clicked));
+    loopThroughViews('saved');
   }
 }
 var $saveButton = document.querySelector('.save-button');
 $saveButton.addEventListener('click', saveWeapon);
+
+function deleteWeapon(event) {
+  if (event.target.matches('.delete-button')) {
+    var $weaponToBeDeleted = event.target.closest('.green-card');
+    var $weaponToBeDeletedID = parseInt($weaponToBeDeleted.getAttribute('id'));
+    for (var savedIndex = 0; savedIndex < data.saved.length; savedIndex++) {
+      if (data.saved[savedIndex].id === $weaponToBeDeletedID) {
+        data.saved.splice(savedIndex, 1);
+        $savedWeaponList.removeChild($weaponToBeDeleted);
+      }
+    }
+  }
+}
+document.addEventListener('click', deleteWeapon);
